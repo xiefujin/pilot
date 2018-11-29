fc94c593d10da1aa46c8cdebbd0621aa0e116a93
sean <sean@SEAN-PC>
2018-11-29 16:39:55 +0800

    用户登录注册

--- pilot/src/main/java/org/beautiful/pilot/controller/UserController.java
+++ pilot/src/main/java/org/beautiful/pilot/controller/UserController.java
@@ -2,6 +2,8 @@ package org.beautiful.pilot.controller;
 
 
 import org.beautiful.pilot.entity.User;
+import org.beautiful.pilot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,6 +12,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 	
+	@Autowired
+	private  UserService userService;
+	/**
+	 * 登录入口
+	 * @param user
+	 * @return
+	 */
+	@RequestMapping("/register")
+	public String register(User user){
+		return "/user/register";
+	}
+	
+	/**
+	 * 登录入口
+	 * @param user
+	 * @return
+	 */
+	@RequestMapping("/doregister")
+	public String doRegister(User user){
+		userService.addUser(user);
+		return "/user/login";
+	}
 	/**
 	 * 登录入口
 	 * @param user
@@ -26,7 +50,7 @@ public class UserController {
 	 * @return
 	 */
 	@RequestMapping("/dologin")
-	public String dologin(){
+	public String doLogin(){
 		return "/user/test";
 	}
 
--- pilot/src/main/java/org/beautiful/pilot/repository/MenuRepository.java
+++ pilot/src/main/java/org/beautiful/pilot/repository/MenuRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.beautiful.pilot.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-
+	@Query(value = "SELECT menu FROM Role role JOIN role.users user JOIN role.menus menu WHERE user.username=?1")
 	List<Menu> findByUsername(String username);
 
 }
