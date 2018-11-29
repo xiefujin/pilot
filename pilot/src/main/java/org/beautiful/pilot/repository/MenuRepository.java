97c2e8013fe20b7303bfadef0a0406a59427a71a
sean <sean@SEAN-PC>
2018-11-29 17:16:58 +0800

    用户登录注册

--- pilot/src/main/java/org/beautiful/pilot/repository/MenuRepository.java
+++ pilot/src/main/java/org/beautiful/pilot/repository/MenuRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.beautiful.pilot.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-
+	@Query("Select menu From Role role JOIN role.users user JOIN role.menus menu where user.username=?1")
 	List<Menu> findByUsername(String username);
 
 }
--- pilot/src/main/java/org/beautiful/pilot/service/MenuService.java
+++ pilot/src/main/java/org/beautiful/pilot/service/MenuService.java
@@ -3,7 +3,9 @@ package org.beautiful.pilot.service;
 import java.util.List;
 
 import org.beautiful.pilot.entity.Menu;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MenuService {
+	
 	List<Menu> findByUserName(String username);
 }
